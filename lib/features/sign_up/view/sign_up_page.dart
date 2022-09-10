9cced036b0c541585c5d5dc6fd57a831c7fb0545
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-09-10 19:03:03 +0200

    add validation to sign up page

--- lib/features/sign_up/view/sign_up_page.dart
+++ lib/features/sign_up/view/sign_up_page.dart
@@ -3,6 +3,7 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:konfiso/shared/app_colors.dart';
+import 'package:konfiso/shared/app_validators.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
@@ -12,69 +13,127 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+
+  final _formKey = GlobalKey<FormState>();
+  String? _email;
+  String? _password;
+
+  final passwordController = TextEditingController();
+
+  void _submitForm() {
+    final formIsValid = _formKey.currentState!.validate();
+    if (formIsValid) {
+      _formKey.currentState!.save();
+    }
+  }
+
+  @override
+  void dispose() {
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 42),
-        child: Form(
-          child: Column(children: [
-            const SizedBox(
-              height: 168,
-            ),
-            const Text(
-              'K',
-              style: TextStyle(
-                  fontSize: 100,
-                  fontFamily: 'Arial MT Rounded',
-                  color: Color(AppColors.primaryColor),
-                  fontWeight: FontWeight.w700),
-            ),
-            const SizedBox(
-              height: 64,
-            ),
-            TextFormField(
-              decoration:
-                  InputDecoration(hintText: Intl.message('Email address')),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              decoration:
-                  InputDecoration(hintText: Intl.message('Password')),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              decoration:
-              InputDecoration(hintText: Intl.message('Password again')),
-            ),
-            const SizedBox(
-              height: 34,
-            ),
-            ElevatedButton(
-                onPressed: () {}, child: Text(Intl.message('Signup'))),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(Intl.message('If you have an account, '),
-                    style: const TextStyle(
-                        color: AppColors.greyColor, fontSize: 14)),
-                GestureDetector(
-                  onTap: () {
-                    print('login');
-                  },
-                  child: Text(Intl.message('log in'),
-                      style: const TextStyle(color: AppColors.greyDarkerColor)),
-                ),
-              ],
-            ),
-          ]),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 42),
+          child: Form(
+            key: _formKey,
+            child: Column(children: [
+              const SizedBox(
+                height: 168,
+              ),
+              const Text(
+                'K',
+                style: TextStyle(
+                    fontSize: 100,
+                    fontFamily: 'Arial MT Rounded',
+                    color: Color(AppColors.primaryColor),
+                    fontWeight: FontWeight.w700),
+              ),
+              const SizedBox(
+                height: 64,
+              ),
+              TextFormField(
+                decoration:
+                    InputDecoration(hintText: Intl.message('Email address')),
+                validator: (String? value) {
+                  String? errorMessage;
+                  if (AppValidators.required(value)) {
+                    errorMessage = Intl.message('Please write an email address');
+                  } else if (AppValidators.email(value)) {
+                    errorMessage = Intl.message('Please put in a valid email address');
+                  }
+                  return errorMessage;
+                },
+                onSaved: (String? value) {
+                  _email = value;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration:
+                    InputDecoration(hintText: Intl.message('Password')),
+                controller: passwordController,
+                obscureText: true,
+                validator: (String? value) {
+                  String? errorMessage;
+                  const minLength = 6;
+                  if (AppValidators.required(value)) {
+                    errorMessage = Intl.message('Please write a password');
+                  } else if (AppValidators.minLength(value, minLength)) {
+                    errorMessage = Intl.message('Please write at least $minLength characters');
+                  }
+                  return errorMessage;
+                },
+                onSaved: (String? value) {
+                  _password = value;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration:
+                InputDecoration(hintText: Intl.message('Password again')),
+                obscureText: true,
+                validator: (String? value) {
+                  String? errorMessage;
+                  if (AppValidators.passwordMatch(passwordController.text, value)) {
+                    errorMessage = Intl.message('Please write identical passwords');
+                  }
+                  return errorMessage;
+                },
+              ),
+              const SizedBox(
+                height: 34,
+              ),
+              ElevatedButton(
+                  onPressed: _submitForm, child: Text(Intl.message('Signup'))),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(Intl.message('If you have an account, '),
+                      style: const TextStyle(
+                          color: AppColors.greyColor, fontSize: 14)),
+                  GestureDetector(
+                    onTap: () {
+                      print('login');
+                    },
+                    child: Text(Intl.message('log in'),
+                        style: const TextStyle(color: AppColors.greyDarkerColor)),
+                  ),
+                ],
+              ),
+            ]),
+          ),
         ),
       ),
     );
