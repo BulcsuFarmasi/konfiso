fc0028f60dae89831735e18b46bcb19cbe624fd3
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-09-10 12:04:34 +0200

    add login and sign up pages

--- lib/shared/app.dart
+++ lib/shared/app.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:konfiso/features/sign_up/view/sign_up_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Konfiso',
-      home: Container(),
-      );
+      home: SignUpPage(),
+    );
   }
 }
