d01e5dbde962ebf7a57f6a5b0f53b60b9fa70840
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-10-07 19:23:59 +0200

    start implementing storing user

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
@@ -16,9 +17,35 @@ final authServiceProvider = Provider((Ref ref) =>
 class AuthService {
   final Dio _httpClient;
   final FlutterSecureStorage _secureStorage;
+  StoredUser? user;
   static const url = 'https://identitytoolkit.googleapis.com/v1/accounts:';
 
   AuthService(this._httpClient, this._secureStorage);
+
+  void refreshToken() async {
+    final response = await _httpClient.post(
+        'https://securetoken.googleapis.com/v1/token?key=$firebaseApiKey',
+        data: jsonEncode({
+          'grant_type': 'refresh_token',
+          'refresh_token': user!.refreshToken
+        }));
+  }
+
+  Future<void> signIn(String email, String password) async {
+    try {
+      final response =
+          await _httpClient.post('${url}signInWithPassword?key=$firebaseApiKey',
+              data: jsonEncode({
+                'email': email,
+                'password': password,
+                'returnSecureToken': true,
+              }));
+      _saveUserSignIn(response.data);
+      Timer(const Duration(minutes: 1), refreshToken);
+    } on DioError catch (e) {
+      throw NetworkException(e.response!.data["error"]["message"]);
+    }
+  }
 
   Future<void> signUp(String email, String password) async {
     try {
@@ -33,29 +60,14 @@ class AuthService {
     }
   }
 
-  Future<void> signIn(String email, String password) async {
-    try {
-      final response =
-          await _httpClient.post('${url}signInWithPassword?key=$firebaseApiKey',
-              data: jsonEncode({
-                'email': email,
-                'password': password,
-                'returnSecureToken': true,
-              }));
-      _saveUserSignIn(response.data);
-    } on DioError catch (e) {
-      throw NetworkException(e.response!.data["error"]["message"]);
-    }
-  }
-
   void _saveUserSignIn(Map<String, dynamic> userSignIn) {
-    final storedUser = StoredUser(
+    user = StoredUser(
         userId: userSignIn['localId'],
         token: userSignIn['idToken'],
         refreshToken: userSignIn['refreshToken'],
         validUntil: DateTime.now()
             .add(Duration(seconds: int.parse(userSignIn['expiresIn']))));
     _secureStorage.write(
-        key: storedUserKey, value: jsonEncode(storedUser.toJson()));
+        key: storedUserKey, value: jsonEncode(user!.toJson()));
   }
 }
