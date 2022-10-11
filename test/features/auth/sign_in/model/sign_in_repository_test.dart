070140d23a73eef4d493e07b7414ac8ca1027388
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-10-11 14:49:44 +0200

    create first 5 unit tests

--- test/features/auth/sign_in/model/sign_in_repository_test.dart
+++ test/features/auth/sign_in/model/sign_in_repository_test.dart
@@ -1,17 +1,76 @@
 @TestOn('vm')
 import 'package:flutter_test/flutter_test.dart';
 import 'package:konfiso/features/auth/services/auth_service.dart';
+import 'package:konfiso/features/auth/sign_in/model/sign_in_error.dart';
+import 'package:konfiso/features/auth/sign_in/model/sign_in_repository.dart';
+import 'package:konfiso/features/auth/sign_in/model/sign_up_expection.dart';
+import 'package:konfiso/shared/expcetions/network_execption.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
 @GenerateNiceMocks([MockSpec<AuthService>()])
-import 'sign_in_repository.mocks.dart';
+import 'sign_in_repository_test.mocks.dart';
 
-void main () {
+void main() {
   group('SignInRepository', () {
     group('signIn', () {
-      test('should call authService signIn', () {
-      print('a');
+      late AuthService authService;
+      late SignInRepository signInRepository;
+      late String email;
+      late String password;
+      setUp(() {
+        authService = MockAuthService();
+        signInRepository = SignInRepository(authService);
+        email = 'test@test.com';
+        password = '123456';
       });
+      test('should call authService signIn', () {
+        signInRepository.signIn(email, password);
+
+        verify(authService.signIn(email, password));
+      });
+      test('should throw invalid email exception if email is invalid', () {
+        when(authService.signIn(email, password))
+            .thenThrow(NetworkException('INVALID_EMAIL'));
+
+        expect(
+            signInRepository.signIn(email, password),
+            throwsA(predicate((e) =>
+                e is SignInException && e.error == SignInError.invalidEmail)));
+      });
+      test('should throw invalid password exception if password is invalid',
+          () {
+        when(authService.signIn(email, password))
+            .thenThrow(NetworkException('INVALID_PASSWORD'));
+
+        expect(
+            signInRepository.signIn(email, password),
+            throwsA(predicate((e) =>
+                e is SignInException &&
+                e.error == SignInError.invalidPassword)));
+      });
+      test('should throw iuser disabled exception if user is disabled',
+          () {
+        when(authService.signIn(email, password))
+            .thenThrow(NetworkException('USER_DISABLED'));
+
+        expect(
+            signInRepository.signIn(email, password),
+            throwsA(predicate((e) =>
+                e is SignInException &&
+                e.error == SignInError.userDisabled)));
+      });
+      test('should throw other exception if the error is unkown',
+              () {
+            when(authService.signIn(email, password))
+                .thenThrow(NetworkException('a'));
+
+            expect(
+                signInRepository.signIn(email, password),
+                throwsA(predicate((e) =>
+                e is SignInException &&
+                    e.error == SignInError.other)));
+          });
     });
   });
 }
