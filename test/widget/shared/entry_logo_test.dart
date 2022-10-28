8a39c8833af16cfae0f896018033b7a201799968
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-10-28 22:14:48 +0200

    add entry logo widget test

---
+++ test/widget/shared/entry_logo_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:konfiso/shared/widgets/entry_in_progress.dart';
+
+void main() {
+  group('EntryLogo', () {
+    Widget createWidgetUnderTest() {
+      return const MaterialApp(
+        home: EntryInProgress(),
+      );
+    }
+
+    testWidgets('should a text with a K in it',
+            (WidgetTester widgetTester) async {
+          await widgetTester.pumpWidget(createWidgetUnderTest());
+
+          expect(find.text('K'), findsOneWidget);
+        });
+  });
+}
