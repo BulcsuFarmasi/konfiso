7516b2448d37e7fa4e50eb4dd2a9486e57ebd443
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-10-28 22:06:38 +0200

    add entry in progress widget test

---
+++ test/widget/shared/entry_in_progress_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:konfiso/shared/widgets/entry_in_progress.dart';
+import 'package:konfiso/shared/widgets/entry_logo.dart';
+
+void main() {
+  group('EntryInProgress', () {
+    Widget createWidgetUnderTest() {
+      return const MaterialApp(
+        home: EntryInProgress(),
+      );
+    }
+
+    testWidgets('should contain entry logo and a circular progress indicator',
+        (WidgetTester widgetTester) async {
+      await widgetTester.pumpWidget(createWidgetUnderTest());
+
+      expect(find.byType(EntryLogo), findsOneWidget);
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    });
+  });
+}
