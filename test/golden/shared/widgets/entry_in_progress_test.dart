b2e8febe9f5bc4cf4951c246779585e6708f352f
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-10-28 21:19:32 +0200

    clean up entry in progress golden test

--- test/golden/shared/widgets/entry_in_progress_test.dart
+++ test/golden/shared/widgets/entry_in_progress_test.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:konfiso/features/auth/sign_in/view/pages/sign_in_page.dart';
 import 'package:konfiso/shared/widgets/entry_in_progress.dart';
 
 void main() {
 
   group('EntryInProgress', () {
-
-    void createWidgetUnderTest() {
-
-    }
 
     testWidgets('should match golden image', (WidgetTester widgetTester) async {
       await widgetTester.pumpWidget(const MaterialApp(home: EntryInProgress(),));
