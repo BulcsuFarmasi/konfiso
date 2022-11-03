2bd1831b9b8c2f4edccbdbe1bf2b23fc8c36482e
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-11-03 14:30:47 +0100

    create function to elminate duplication

--- integration_test/integration/features/auth/sign_in/view/pages/sign_in_page_test.dart
+++ integration_test/integration/features/auth/sign_in/view/pages/sign_in_page_test.dart
@@ -43,53 +43,7 @@ void main() {
           }));
     }
 
-    setUp(() {
-      signInPageStateNotifier =
-          MockSignInPageStateNotifier(const SignInPageState.initial());
-
-      email = 'test@test.com';
-      password = '123456';
-    });
-
-    testWidgets('initially should be in initial state',
-            (WidgetTester widgetTester) async {
-          await widgetTester.pumpWidget(createWidgetUnderTest());
-
-          expect(find.byType(SignInInitial), findsOneWidget);
-        });
-
-
-    testWidgets(
-        'should navigate to books home page when the correct sign in data was provided',
-        (WidgetTester widgetTester) async {
-       await widgetTester.pumpWidget(createWidgetUnderTest());
-
-      final emailField = find.byKey(SignInForm.emailKey);
-      final passwordField = find.byKey(SignInForm.passwordKey);
-      final button = find.byType(ElevatedButton);
-
-      await widgetTester.enterText(emailField, email);
-      await widgetTester.enterText(passwordField, password);
-       await widgetTester.testTextInput.receiveAction(TextInputAction.done);
-       await widgetTester.pumpAndSettle();
-
-      await widgetTester.tap(button);
-
-      signInPageStateNotifier.state = const SignInPageState.inProgress();
-
-      await widgetTester.pump();
-
-      expect(find.byType(EntryInProgress), findsOneWidget);
-
-      signInPageStateNotifier.state = const SignInPageState.successful();
-
-      await widgetTester.pumpAndSettle();
-
-      expect(find.byType(BookHomePage), findsOneWidget);
-    });
-
-    testWidgets('should display error and error bannner widgets',
-        (WidgetTester widgetTester) async {
+    Future<void> testUntilInProgress(WidgetTester widgetTester) async {
       await widgetTester.pumpWidget(createWidgetUnderTest());
 
       final emailField = find.byKey(SignInForm.emailKey);
@@ -108,6 +62,38 @@ void main() {
       await widgetTester.pump();
 
       expect(find.byType(EntryInProgress), findsOneWidget);
+    }
+
+    setUp(() {
+      signInPageStateNotifier =
+          MockSignInPageStateNotifier(const SignInPageState.initial());
+
+      email = 'test@test.com';
+      password = '123456';
+    });
+
+    testWidgets('initially should be in initial state',
+        (WidgetTester widgetTester) async {
+      await widgetTester.pumpWidget(createWidgetUnderTest());
+
+      expect(find.byType(SignInInitial), findsOneWidget);
+    });
+
+    testWidgets(
+        'should navigate to books home page when the correct sign in data was provided',
+        (WidgetTester widgetTester) async {
+      await testUntilInProgress(widgetTester);
+
+      signInPageStateNotifier.state = const SignInPageState.successful();
+
+      await widgetTester.pumpAndSettle();
+
+      expect(find.byType(BookHomePage), findsOneWidget);
+    });
+
+    testWidgets('should display error and error bannner widgets',
+        (WidgetTester widgetTester) async {
+      await testUntilInProgress(widgetTester);
 
       signInPageStateNotifier.state =
           const SignInPageState.error(model_sign_in_error.SignInError.other);
@@ -171,23 +157,23 @@ void main() {
       });
       testWidgets(
           'should check if password is put in, validation message is NOT displayed',
-              (WidgetTester widgetTester) async {
-            await widgetTester.pumpWidget(createWidgetUnderTest());
+          (WidgetTester widgetTester) async {
+        await widgetTester.pumpWidget(createWidgetUnderTest());
 
-            final passwordField = find.byKey(SignInForm.passwordKey);
+        final passwordField = find.byKey(SignInForm.passwordKey);
 
-            final button = find.byType(ElevatedButton);
+        final button = find.byType(ElevatedButton);
 
-            await widgetTester.enterText(passwordField, password);
-            await widgetTester.testTextInput.receiveAction(TextInputAction.done);
-            await widgetTester.pumpAndSettle();
+        await widgetTester.enterText(passwordField, password);
+        await widgetTester.testTextInput.receiveAction(TextInputAction.done);
+        await widgetTester.pumpAndSettle();
 
-            await widgetTester.tap(button);
+        await widgetTester.tap(button);
 
-            await widgetTester.pumpAndSettle();
+        await widgetTester.pumpAndSettle();
 
-            expect(find.text('Please write an password'), findsNothing);
-          });
+        expect(find.text('Please write an password'), findsNothing);
+      });
     });
   });
 }
