e698c5f64a376cbb55e56d41aaf4ed2e728a3f97
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-11-05 19:17:53 +0100

    add golden test for app

---
+++ test/golden/shared/widgets/app_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:konfiso/shared/widgets/app.dart';
+
+void main() {
+  group('App', () {
+    Widget createWidgetUnderTest() {
+      return const App();
+    }
+
+    testWidgets('should match golden image', (WidgetTester widgetTester) async {
+      await widgetTester.pumpWidget(createWidgetUnderTest());
+
+      expect(find.byType(App),
+          matchesGoldenFile('app_test.png'));
+    });
+  });
+}
