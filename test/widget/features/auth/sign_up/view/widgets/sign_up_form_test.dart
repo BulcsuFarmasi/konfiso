713679851c1b9d79d4a98fb9815c3e6aa3df6f3a
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-11-05 23:04:39 +0100

    add widget test for sign up form

---
+++ test/widget/features/auth/sign_up/view/widgets/sign_up_form_test.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:konfiso/features/auth/sign_up/view/widgets/sign_up_form.dart';
+
+void main() {
+  group('SignUpForm', () {
+    Widget createWidgetUnderTest() {
+      return const MaterialApp(
+        home: Scaffold(
+          body: SignUpForm(),
+        ),
+      );
+    }
+
+    testWidgets('should find a form', (widgetTester) async {
+      await widgetTester.pumpWidget(createWidgetUnderTest());
+
+      expect(find.byType(Form), findsOneWidget);
+    });
+    testWidgets('should find three text form fields', (widgetTester) async {
+      await widgetTester.pumpWidget(createWidgetUnderTest());
+
+      expect(find.byType(TextFormField), findsNWidgets(3));
+    });
+    testWidgets('should find a button', (widgetTester) async {
+      await widgetTester.pumpWidget(createWidgetUnderTest());
+
+      expect(find.byType(ElevatedButton), findsOneWidget);
+    });
+
+    testWidgets('should the text and the link up it', (widgetTester) async {
+      await widgetTester.pumpWidget(createWidgetUnderTest());
+
+      expect(find.text('If you have an account, '), findsOneWidget);
+      expect(
+          find.descendant(
+            of: find.byType(GestureDetector),
+            matching: find.text('log in'),
+          ),
+          findsOneWidget);
+    });
+  });
+}
