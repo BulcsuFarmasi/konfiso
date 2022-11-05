fa304a948553e4e14eeec0a5876be5a9c0a53f05
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-11-06 00:14:37 +0100

    add flavor config and different starting points

--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:konfiso/shared/widgets/app.dart';
-
-void main() {
-  runApp(const App());
-}
--- lib/shared/secret.dart
+++ lib/shared/secret.dart
@@ -1,1 +1,2 @@
-const firebaseApiKey = 'AIzaSyAx0hpYb3A6hZzuWc2f7ML2cW3AlPmibRQ';
+const devFirebaseApiKey = 'AIzaSyAx0hpYb3A6hZzuWc2f7ML2cW3AlPmibRQ';
+const prodFirsebaseApiKey = 'AIzaSyAZ95TRVP62Mj3_9uaxWMhRxKcI2LR-uuk';
