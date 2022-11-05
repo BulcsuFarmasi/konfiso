85726804305910778d6a88ad7df347d5d04355f1
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-11-05 23:27:54 +0100

    add widget test for books home page

---
+++ test/widget/features/books/view/pages/books_home_page_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:konfiso/features/book/book_home/view/book_home_page.dart';
+
+void main() {
+  group('BooksHomePage', () {
+    Widget createWidgetUnderTest() {
+      return const MaterialApp(home: BookHomePage(),);
+    }
+    testWidgets(
+        'should contain the books text', (WidgetTester widgetTester) async {
+      await widgetTester.pumpWidget(createWidgetUnderTest());
+
+      expect(find.text('Books'), findsOneWidget);
+    });
+  });
+
+}
