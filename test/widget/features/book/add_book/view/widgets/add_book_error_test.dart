fe0765d4f1d481d11dd20f749814415ce91fdf97
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-11-17 19:28:52 +0100

    add widget test for add book in progress

--- test/widget/features/book/add_book/view/widgets/add_book_error_test.dart
+++ test/widget/features/book/add_book/view/widgets/add_book_error_test.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:konfiso/features/book/add_book/view/widgets/add_book_error.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   group('AddBookError', () {
     Widget createWidgetUnderTest() {
-      return const MaterialApp(home: AddBookError());
+      return const MaterialApp(
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          home: AddBookError());
     }
+
     testWidgets('should contain error text', (WidgetTester widgetTester) async {
       await widgetTester.pumpWidget(createWidgetUnderTest());
-      
+
       // check for error message
-      expect(find.text('An error occured, couldn\'t load books'), findsOneWidget);
+      expect(
+          find.text('An error occured, couldn\'t load books'), findsOneWidget);
     });
   });
-  
 }
