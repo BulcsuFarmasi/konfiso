90020e5cca3015d9e8a2c8bea2d2702fe3bc5841
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-11-17 20:07:17 +0100

    add integration and widget test to add book search

--- integration_test/features/book/book_detail/view/pages/book_detail_page_test.dart
+++ integration_test/features/book/book_detail/view/pages/book_detail_page_test.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:konfiso/features/book/book_detail/controller/book_detail_page_state.dart';
 import 'package:konfiso/features/book/book_detail/controller/book_detail_page_state_notifier.dart';
 import 'package:konfiso/features/book/book_detail/view/pages/book_detail_page.dart';
 import 'package:konfiso/features/book/book_detail/view/widgets/book_detail_in_progress.dart';
 import 'package:konfiso/features/book/book_detail/view/widgets/book_detail_success.dart';
 import 'package:konfiso/features/book/model/book.dart';
-import 'package:konfiso/features/book/model/book_ids.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockBookDetailPageStateNotifier extends StateNotifier<BookDetailPageState>
---
+++ test/widget/features/book/add_book/view/widgets/add_book_search_test.dart
@@ -0,0 +1,31 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:konfiso/features/book/add_book/view/widgets/add_book_search.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+void main() {
+  group('AddBookSearch', () {
+    Widget createWidgetUnderTest() {
+      return ProviderScope(
+        child: MaterialApp(
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          home: Scaffold(
+            body: AddBookSearch(startedTyping: () {}),
+          ),
+        ),
+      );
+    }
+
+    testWidgets('should contain a text field',
+        (WidgetTester widgetTester) async {
+      await widgetTester.pumpWidget(createWidgetUnderTest());
+
+      // assert if contains a text field
+      expect(find.byType(TextField), findsOneWidget);
+    });
+  });
+}
