b29055dd677f9fa8ae5604a983485229581e3dda
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-11-17 19:24:12 +0100

    add widget test for add book error

---
+++ test/widget/features/book/add_book/view/widgets/add_book_error.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:konfiso/features/book/add_book/view/widgets/add_book_error.dart';
+
+void main() {
+  group('AddBookError', () {
+    Widget createWidgetUnderTest() {
+      return const MaterialApp(home: AddBookError());
+    }
+    testWidgets('should contain error text', (WidgetTester widgetTester) async {
+      await widgetTester.pumpWidget(createWidgetUnderTest());
+      
+      // check for error message
+      expect(find.text('An error occured, couldn\'t load books'), findsOneWidget);
+    });
+  });
+  
+}
