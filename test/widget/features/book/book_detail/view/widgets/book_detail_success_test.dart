72741c7d7cb97a396a68df5846e0e126aef94a9b
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-11-18 16:05:52 +0100

    add book detail success widget test

---
+++ test/widget/features/book/book_detail/view/widgets/book_detail_success_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:konfiso/features/book/book_detail/view/widgets/book_data.dart';
+import 'package:konfiso/features/book/book_detail/view/widgets/book_detail_success.dart';
+import 'package:konfiso/features/book/book_detail/view/widgets/book_reading_detail_form.dart';
+import 'package:konfiso/features/book/model/book.dart';
+
+void main() {
+  group('BookDetailSuccess', () {
+    Widget createWidgetUnderTest() {
+      return const MaterialApp(
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: Scaffold(
+          body: SingleChildScrollView(
+            child: BookDetailSuccess(
+              book: Book(title: 'a', externalId: 'b'),
+            ),
+          ),
+        ),
+      );
+    }
+
+    testWidgets('should display book data', (WidgetTester widgetTester) async {
+      await widgetTester.pumpWidget(createWidgetUnderTest());
+
+      expect(find.byType(BookData), findsOneWidget);
+    });
+
+    testWidgets('should display book reading detail form',
+        (WidgetTester widgetTester) async {
+      await widgetTester.pumpWidget(createWidgetUnderTest());
+
+      expect(find.byType(BookReadingDetailForm), findsOneWidget);
+    });
+  });
+}
