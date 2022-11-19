dafd9c65d1e476144f2a71c23101ab637239f57d
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-11-19 14:53:38 +0100

    add page for forgotten password

---
+++ test/unit/features/auth/forgotten_password/view/page/forgotten_password_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ForgottenPasswordPage extends StatelessWidget {
+  const ForgottenPasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
