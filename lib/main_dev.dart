31c23055d75b00e75fd0c6e5eb37d12011c21bf9
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-11-20 10:27:11 +0100

    move app widget into it own folder

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:konfiso/shared/widgets/app.dart';
+import 'package:konfiso/shared/widgets/app/view/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
--- test/golden/shared/widgets/app_test.dart
+++ test/golden/shared/widgets/app_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:konfiso/shared/widgets/app.dart';
+import 'package:konfiso/shared/widgets/app/view/app.dart';
 
 void main() {
   group('App', () {
