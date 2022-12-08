2ce2edc4dca2ce4a485ce07fca5ba22916f4356b
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-12-08 13:20:15 +0100

    add privacy policy service

---
+++ lib/shared/services/privacy_poilcy_service.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:konfiso/shared/services/language_service.dart';
+
+final privacyPolicyServiceProvider = Provider((Ref ref) => PrivacyPolicyService(ref.read(languageServiceProvider)));
+
+class PrivacyPolicyService {
+  final LanguageService languageService;
+
+  static const versionNumber = 1;
+
+  PrivacyPolicyService(this.languageService);
+
+  String get privacyPolicyUrl => 'https://konfiso.com/privacy-policy/${languageService.locale}/v$versionNumber/privacy-policy.pdf';
+}
