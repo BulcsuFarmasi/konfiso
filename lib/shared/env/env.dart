4811adcec21f287b29bde6d56476c1052fbea617
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-12-30 12:56:03 +0100

    obfucate env variables

---
+++ lib/shared/env/env.dart
@@ -0,0 +1,21 @@
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied()
+class Env {
+  @EnviedField(varName: 'DEV_GOOGLE_API_KEY', obfuscate: true)
+  static final devGoogleApiKey = _Env.devGoogleApiKey;
+
+  @EnviedField(varName: 'DEV_FIREBASE_DB_URL', obfuscate: true)
+  static final devFirebaseDBUrl = _Env.devFirebaseDBUrl;
+
+  @EnviedField(varName: 'PROD_GOOGLE_API_KEY', obfuscate: true)
+  static final prodGoogleApiKey = _Env.prodGoogleApiKey;
+
+  @EnviedField(varName: 'DEV_FIREBASE_DB_URL', obfuscate: true)
+  static final prodFirebaseDBUrl = _Env.prodFirebaseDBUrl;
+
+  @EnviedField(varName: 'MOLY_API_KEY', obfuscate: true)
+  static final molyApiKey = _Env.molyApiKey;
+}
--- lib/shared/utils/flavor_util.dart
+++ lib/shared/utils/flavor_util.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:konfiso/shared/env/env.dart';
 import 'package:konfiso/shared/flavor.dart';
 import 'package:konfiso/shared/flavor_config.dart';
-import 'package:konfiso/env.dart';
 
 final flavorUtilProvider = Provider((_) => FlavorUtil());
 
@@ -9,11 +9,11 @@ class FlavorUtil {
   FlavorConfig get currentConfig {
     switch (flavor) {
       case Flavor.dev:
-        return FlavorConfig(Flavor.dev, FlavorValues(devGoogleApiKey, devFirebaseDBUrl));
+        return FlavorConfig(Flavor.dev, FlavorValues(Env.devGoogleApiKey, Env.devFirebaseDBUrl));
       case Flavor.staging:
-        return FlavorConfig(Flavor.staging, FlavorValues(prodGoogleApiKey, prodFirebaseDBUrl));
+        return FlavorConfig(Flavor.staging, FlavorValues(Env.prodGoogleApiKey, Env.prodFirebaseDBUrl));
       case Flavor.prod:
-        return FlavorConfig(Flavor.prod, FlavorValues(prodGoogleApiKey, prodFirebaseDBUrl));
+        return FlavorConfig(Flavor.prod, FlavorValues(Env.prodGoogleApiKey, Env.prodFirebaseDBUrl));
     }
   }
 }
