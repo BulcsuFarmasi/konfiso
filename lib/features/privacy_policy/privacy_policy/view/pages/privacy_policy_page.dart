b14535985c8a4a3cae92f54c877bd60f2c31b307
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2023-02-02 10:46:27 +0100

    load pdf via flutter cached pdf view

--- lib/features/privacy_policy/privacy_policy/view/pages/privacy_policy_page.dart
+++ lib/features/privacy_policy/privacy_policy/view/pages/privacy_policy_page.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:konfiso/features/privacy_policy/privacy_policy/controller/privacy_policy_page_state_notifier.dart';
+import 'package:konfiso/features/privacy_policy/privacy_policy/view/widgets/privacy_policy_successful.dart';
 
-class PrivacyPolicyPage extends StatelessWidget {
+class PrivacyPolicyPage extends ConsumerStatefulWidget {
   const PrivacyPolicyPage({super.key});
+
+  @override
+  ConsumerState<PrivacyPolicyPage> createState() => _PrivacyPolicyPageState();
+}
+
+class _PrivacyPolicyPageState extends ConsumerState<PrivacyPolicyPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future(() {
+      ref.read(privacyPolicyPageStateNotifierProvider.notifier).loadPrivacyPolicyUrl();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Consumer(builder: (_, WidgetRef ref, __) {
-        return Container();
-      },),
+      body: Consumer(
+        builder: (_, WidgetRef ref, __) {
+          final state = ref.watch(privacyPolicyPageStateNotifierProvider);
+          return state.maybeMap(
+            successful: (successful) => PrivacyPolicySuccessful(successful.privacyPolicyUrl),
+            orElse: () => const SizedBox(),
+          );
+        },
+      ),
     );
   }
 }
