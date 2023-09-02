6efb8a2ff2933a72ab7640df3a6c98f1f940e967
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2023-09-02 14:07:22 +0200

    change forgetten password to use native switch

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -142,8 +142,8 @@ class AuthService {
   Future<void> sendPasswordResetEmail(String email) async {
     try {
       await _authApiRemote.sendPasswordResetEmail(email);
-    } on DioError catch (e) {
-      final errorMessage =
+    } on DioException catch (e) {
+      final String errorMessage =
           e.response!.data['error'] is! String ? e.response!.data['error']['message'] : e.response!.data['error'];
       throw NetworkException(errorMessage);
     }
